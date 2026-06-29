68ee2791e47c8948435a70be4de73908f329ad6e
Taakshini <taakshini2005@gmail.com>
2026-06-29 12:04:28 +0530

    folders added along with progress

---
+++ PYTHON/file.py
@@ -0,0 +1,5 @@
+f=open("abc.txt","w")
+f.write("hello world")
+f.write("Sarah,Jeon kim ")
+
+f.close()
---
+++ word_count/word_count.py
@@ -0,0 +1,4 @@
+with open("sample.txt","w") as f:
+    f.write("hello world ")
+
+with open("sample.txt","r")       
